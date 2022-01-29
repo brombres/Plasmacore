dcfc3d51f7dfa1754c696dcf1a4bd65b2396e20b
Abe Pralle <abe.pralle@gmail.com>
2022-01-28 20:09:02 -1000

    [macOS] New project

---
+++ Platforms/macOS/Project-macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Project-macOS
+//
+//  Created by Abe Pralle on 1/28/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
