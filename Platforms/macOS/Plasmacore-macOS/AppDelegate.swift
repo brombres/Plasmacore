145ccf283de1a2cc492388ee294ba46fc515ed9c
Brom Bresenham <brom.bresenham@runegate.com>
2023-04-14 13:32:07 -1000

    [macOS] Created Xcode 'Game' starter project

---
+++ Platforms/macOS/Plasmacore-macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Plasmacore-macOS
+//
+//  Created by Abe Pralle on 4/14/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
