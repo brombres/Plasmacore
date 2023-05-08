690c4f29fe9fed3ae4b972b172bbc94745c04cd0
Brom Bresenham <brom.bresenham@runegate.com>
2023-05-08 14:30:25 -0700

    [Vulkan] Refactor

--- Libraries/Framework/Swift/CubeWrapper.c
+++ Libraries/Framework/Swift/CubeWrapper.c
@@ -11,6 +11,7 @@ void call_demo_main( int argc, const char *argv[] )
 {
   //demo_main( &cube_demo, caMetalLayer, argc, argv );
   demo_init(&cube_demo, argc, (char **)argv);
+  cube_demo.caMetalLayer = ROGUE_SINGLETON(PlasmacoreMoltenVKRenderer)->ca_metal_layer;
   demo_init_vk_swapchain( &cube_demo );
   demo_prepare( &cube_demo );
   cube_demo.spin_angle = 0.4f;
